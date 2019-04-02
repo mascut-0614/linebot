3a7975782da5aff77aa7fe8e99b02125a56d54cd
mascut-0614 <chocochan_i@dcl.cs.waseda.ac.jp>
2019-04-02 18:50:07 +0900

    fixed

--- callback.php
+++ callback.php
@@ -1,3 +1,4 @@
+<?php
 $access_token='1N3AcyxVAGjcEX7jYzAuRLTbyhCFH84ZOZSvoVogPgIa0VqnJg+eh7q9MrsWUeFKKj1oDSB1qMznR7D66RAiqncYuxrGLnncaGyBG4rR1YJe4Kedi0inAp/eUH8YRIsP3nt/00MzSru9ClXdH17EKwdB04t89/1O/w1cDnyilFU='
 
 $json_string = file_get_contents('php://input');
@@ -34,5 +35,23 @@ if($type=="message"){
         } else {
             $message = array(
                 'type' => 'text',
-                'text' => '【'.$msg_text.'】っておいしい？'
-:
+                'text' => '【'.$msg_text.'】っておいしい？');
+        }
+}elseif($type=="postback"){
+  $postback = $json_obj->{'events'}[0]->{'postback'}->{'data'};
+  if($postback === 'datetemp') {
+      $message = array('type' => 'text','text' => '【'.$json_obj->{'events'}[0]->{'postback'}->{'params'}->{'date'}.'】にご予約を承りました。');
+  }elseif($postback === 'action=back'){
+      $message=array('type'=> 'text','text'=>'何もしませんでした。');
+  }
+}
+$postdata=array('replyToken'=>$reply_token,'messages'=>array($massage));
+$ch = curl_init('https://api.line.me/v2/bot/message/reply');
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charser=UTF-8','Authorization: Bearer ' . $access_token));
+$result = curl_exec($ch);
+curl_close($ch);
+?>
